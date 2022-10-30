710fc5d9601f263d8595704c9fb64f4fd3870505
Tasnimul Hasan Samit <57057963+samithasan142@users.noreply.github.com>
2022-10-30 17:46:43 +0600

    Add files via upload

---
+++ Beginners Python/10. Relational Operator.py
@@ -0,0 +1,9 @@
+# Relational Operator & Boolean Data Type (Tutorial 12)
+
+print(30>20)
+print(30<20)
+print(30>=20)
+print(30<=20)
+print(30==20)
+print(30!=20)
+print("Anis"=="Anis")
---
+++ Beginners Python/16. Letter Grade Program.py
@@ -0,0 +1,7 @@
+# Letter Grade Program (Tutorial 19)
+
+marks = 85
+if 80 <= marks <= 100:
+    print("A+")
+elif marks >= 70 and marks <= 79:
+    print("A")
