d548fe8cc25122b72bfeaac41e92c61727166983
Tasnimul Hasan Samit <57057963+samithasan142@users.noreply.github.com>
2022-10-31 12:55:02 +0600

    Delete Beginners Python directory

--- Beginners Python/10. Relational Operator.py
+++
@@ -1,9 +0,0 @@
-# Relational Operator & Boolean Data Type (Tutorial 12)
-
-print(30>20)
-print(30<20)
-print(30>=20)
-print(30<=20)
-print(30==20)
-print(30!=20)
-print("Anis"=="Anis")
--- Beginners Python/2. Backslash Characters.py
+++
@@ -1,5 +0,0 @@
-# Backslash Characters (Tutorial 4)
-
-print("Samit \n Aust")
-print("Samit \t Aust")
-print("\"Samit\"") #print double quotation
